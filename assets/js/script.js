dbea17c04b99ffc09a87cc2e88236e2238ba9746
Didier Krux <didier@krux.co>
2018-03-14 18:14:32 +0800

    optimise img size and compression with ImageOptim

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,49 +3,49 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
     // delay 2 sec for CSS typing
-    if($('.typewriter h2').css('display') == 'none')
+    if ($('.typewriter h2').css('display') == 'none')
         $('.typewriter h2').delay(2000).show(0);
-})
 
-// smooth scroll animation
-$('.navbar a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
-    // On-page links
-    if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        &&
-        location.hostname == this.hostname
-    ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-            // Only prevent default if animation is actually gonna happen
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top-48
-            }, 600, function () {
-                // Callback after animation
-                // Must change focus!
-                var $target = $(target);
-                $target.focus();
-                if ($target.is(":focus")) { // Checking if the target was focused
-                    return false;
-                } else {
-                    $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                    $target.focus(); // Set focus again
-                };
-            });
+    // smooth scroll animation
+    $('.navbar a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 48
+                }, 600, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
         }
-    }
-});
+    });
 
-// toggle active scroll top
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 0) {
-        $('.navbar').addClass('active-top');
-    } else {
-        $('.navbar').removeClass('active-top');
-    }
+    // toggle active scroll top
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 0) {
+            $('.navbar').addClass('active-top');
+        } else {
+            $('.navbar').removeClass('active-top');
+        }
 
-}).scroll();
+    }).scroll();
+})
