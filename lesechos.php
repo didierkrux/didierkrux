c4367c736fa43cce079915527ee285f27fbc7a8d
Didier Krux <didier@krux.co>
2018-09-04 11:13:59 +0200

    Les Echos.fr redirection linkedin

---
+++ lesechos.php
@@ -0,0 +1,18 @@
+<?
+if (strpos($_SERVER['HTTP_REFERER'], 'linkedin') !== false) {
+    header('Location: https://krux.co/resume'); // go to website if you come from linkedin
+} else { // display og:image, title & description for linkedin profile
+    $title = 'Les Echos.fr - Actualité économique, financière';
+    $description = 'Toute l&#039;actualité économique, financière et boursière française et internationale sur Les Echos.fr';
+    $img = 'https://www.didierkrux.com/assets/img/lesechos.fr.png';
+    echo '<html>
+    <head>
+        <title>' . $title . '</title>
+        <meta name="description" content="' . $description . '" />
+        <meta property="og:image" content="' . $img . '" />
+    </head>
+    <body>
+    </body>
+    </html>';
+}
+exit;
