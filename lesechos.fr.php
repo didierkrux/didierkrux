13c83acbcf5568f0cc53ba28ac3ea19e4b643511
Didier Krux <didier@krux.co>
2018-09-04 11:16:50 +0200

    fix url

--- lesechos.fr.php
+++ lesechos.fr.php
@@ -1,6 +1,6 @@
 <?
 if (strpos($_SERVER['HTTP_REFERER'], 'linkedin') !== false) {
-    header('Location: https://krux.co/resume'); // go to website if you come from linkedin
+    header('Location: https://www.lesechos.fr/'); // go to website if you come from linkedin
 } else { // display og:image, title & description for linkedin profile
     $title = 'Les Echos.fr - Actualité économique, financière';
     $description = 'Toute l&#039;actualité économique, financière et boursière française et internationale sur Les Echos.fr';
