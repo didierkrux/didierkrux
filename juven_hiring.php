f785d93fd3fea595b187ad16863c29b7411baf05
Didier Krux <didier@krux.co>
2018-11-26 19:41:28 +0800

    emoji

--- juven_hiring.php
+++ juven_hiring.php
@@ -2,7 +2,7 @@
 if (strpos($_SERVER['HTTP_REFERER'], 'linkedin') !== false) {
     header('Location: https://www.linkedin.com/jobs/search/?f_C=13440071'); // go to website if you come from linkedin
 } else { // display og:image, title & description for linkedin profile
-    $title = 'Juven is hiring!';
+    $title = '🚀 Juven is hiring! 👨‍💻';
     $description = 'Want to join our growing team? Juven is hiring senior developers, apply here!';
     $img = 'https://www.didierkrux.com/assets/img/talent_wanted.png';
     echo '<html>
