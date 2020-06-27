666dfe7ef989fd2b221b30a70e7ae598ce1070e1
Didier Krux <didier@krux.co>
2020-06-27 18:20:35 +0800

    clean

--- resume.php
+++
@@ -1,7 +0,0 @@
-<?
-if (strpos($_SERVER['HTTP_REFERER'], 'linkedin') === false) {
-    header('Location: resume.html');    // display og:image for linkedin profile
-} else {
-    header('Location: https://krux.co/resume');     // go to dl page if you come from linkedin
-}
-exit;
